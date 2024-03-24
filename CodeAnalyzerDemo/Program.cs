217ab0535f6f2ab6db60cebeb202dafebbd5c929
s-toshev <simeon.toshev@yahoo.bg>
2024-03-24 20:03:20 +0200

    Add project files.

---
+++ CodeAnalyzerDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hi!"); 
+    }
+
+    private string MakeGreeting(string name)
+    {
+        if (name == null) // No braces and spacing issues
+            throw new ArgumentNullException("name");
+        return "Hello, " + name; 
+    }
+}
